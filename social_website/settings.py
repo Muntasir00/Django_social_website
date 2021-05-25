666c0d97ddaa47a333e2bc3125cbee7c34852209
“Muntasir00” <muntasir690000@gmail.com>
2021-05-25 22:34:02 +0600

    password part completeion

--- social_website/settings.py
+++ social_website/settings.py
@@ -138,3 +138,4 @@ STATICFILES_FINDERS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
